75287688e644d7869f2197880b79e66d3051177f
selimmemduhoglu <mselimmemduhoglu@gmail.com>
2024-04-13 00:29:37 +0300

    updated

--- CoreIdentityAuthAndAthorize/Controllers/AuthController.cs
+++ CoreIdentityAuthAndAthorize/Controllers/AuthController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using CoreIdentityAuthAndAthorize.Models.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreIdentityAuthAndAthorize.Controllers
@@ -6,13 +8,22 @@ namespace CoreIdentityAuthAndAthorize.Controllers
     [Authorize]
     public class AuthController : Controller
     {
+        readonly UserManager<AppUser> _userManager;
+
+        public AuthController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult TestAction()
+        public async Task<IActionResult> TestAction()
         {
+            AppUser curUser = await _userManager.GetUserAsync(HttpContext.User);
+            ViewBag.UserName = curUser.UserName;
             return View();
         }
     }
